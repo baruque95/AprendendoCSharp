700bf43553fd43f161b8649269b846b9e3683200
baruque95 <baruquechou@gmail.com>
2023-01-13 15:29:37 -0300

    detalhes sobre atribuição de variaveis

---
+++ 6-Strings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string primeiraFrase = "Aprendendo C# novamente. Mais uma vez. De novo.";
+
+        Console.WriteLine(primeiraFrase);
+
+        string carros = @"Marcas de carro: 
+        - VolksWagen
+        - Ford
+        - Chevrolet
+        - Brasília";
+        Console.WriteLine(carros);
+        /*
+        Observe que ao utilizar o @, pode-se fazer uma string de múltiplas linhas.
+        Os espaços dados à esquerda das linhas abaixo da @ vão mudar o alinhamento
+        das linhas da string, por isso fica esquisito em código. No caso da string
+        carros, cada linha está afastada da margem esquerda do console.
+        */
+    }
+}
---
+++ 7-AtribuiçãoDeVariáveis/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int idade = 27;
+        int idadeGabriel = idade;
+
+        Console.WriteLine(idadeGabriel);
+
+        idade = 30;
+        Console.WriteLine(idade);
+        Console.WriteLine(idadeGabriel);
+
+    }
+}
