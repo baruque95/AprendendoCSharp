5df0e8ad1fbd563e97e54afa761bd5b89905364d
baruque95 <baruquechou@gmail.com>
2023-01-13 11:55:42 -0300

    projeto5 aula5

--- 4-ConversaoDeTipos/Program.cs
+++ 4-ConversaoDeTipos/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double salario = 3000.50;
+        int salarioInteiro;
+
+        /*
+        DE FORMA A ATRIBUIR O VALOR DOUBLE À VARIÁVEL salarioInteiro, DEVE-SE 
+        TRANSFORMAR O TIPO DE DADO DO VALOR DA VARIÁVEL salario. ENTÃO, FAZ-SE
+        DA FORMA COMO SE VÊ ABAIXO.
+        */
+
+        salarioInteiro = (int)salario;
+    }
+}
---
+++ 5-Strings/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        char caractere = 'a';
+        // PERCEBA QUE ATRIBUIÇÃO DE CHAR DEVE SER FEITA COM ASPAS SIMPLES ''
+        Console.WriteLine(caractere);
+
+        char asciiNumerico = (char)70;
+        // NESTE CASO, A ATRIBUIÇÃO É DE UM CARACTERE PELA TABELA ASCII. O OUTPUT É A LETRA 'F'
+        Console.WriteLine(asciiNumerico);
+    }
+}
