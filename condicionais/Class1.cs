a1cdab1bb021838e0b3995c359aae6537ea61677
baruque95 <baruquechou@gmail.com>
2023-01-23 16:42:00 -0300

    Condicionais em C#

---
+++ condicionais/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        double salario;
+        bool foiPromovido = true;
+        if (foiPromovido)
+        {
+            // Caso salario seja declarado dentro do if, o que não foi o caso, ele se torna uma variável temporária.
+            // Isso faria com que ao fim do programa, ao se pedir que imprima o salario, cause erro pois a variável já não existe mais.
+            salario = 4200.0;
+        }
+        else
+        {
+            salario = 3800.0;
+        }
+        Console.WriteLine(salario);
+    }
+}
